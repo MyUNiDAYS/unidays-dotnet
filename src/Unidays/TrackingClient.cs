897a8ef7b10302ad285aaef3b0832766b18b0deb
jessica.white <jessica.white@myunidays.com>
2018-07-18 16:10:05 +0100

    Implement Tracking Client
    To offer the option of us sending requests on behalf of the partners

--- src/Unidays/TrackingClient.cs
+++ src/Unidays/TrackingClient.cs
@@ -1,30 +1,32 @@
 ﻿using System.Net;
 using System.Net.Http;
-using System.Threading;
 using System.Threading.Tasks;
 
 namespace Unidays
 {
-	class TrackingClient
+	public class TrackingClient
     {
-	    const string trackingUrl = "https://tracking.myunidays.com/perks/redemption/v1.1";
+	    const string trackingUrl = "https://tracking.myunidays.com/perks/redemption/v1.2";
 
 		private readonly HttpClient _httpClient;
-	    private readonly string _customerId;
+	    private readonly DirectTrackingDetails _directTrackingDetails;
 	    private readonly byte[] _key;
 
-	    public TrackingClient(HttpClient httpClient, string customerId, byte[] key)
+	    public TrackingClient(HttpClient httpClient, DirectTrackingDetails directTrackingDetails, byte[] key)
 	    {
 		    _httpClient = httpClient;
-		    _customerId = customerId;
+			_directTrackingDetails = directTrackingDetails;
 		    _key = key;
 	    }
 
-	    public async Task<HttpStatusCode> PostAsync(DirectTrackingDetails directTrackingDetails)
+	    /// <summary>
+	    /// Sends a Server-to-Server Redemption Tracking Request
+	    /// </summary>
+	    /// <returns>StatusCode of the resulting call</returns>
+		public async Task<HttpStatusCode> SendAsync()
 	    {
-			var uri = new UriGenerator().GenerateServerRequestUrl(_key, directTrackingDetails);
-
-		    var response = await _httpClient.PostAsync(uri, null, CancellationToken.None);
+			var uri = new UriGenerator().GenerateServerRequestUrl(_key, _directTrackingDetails);
+		    var response = await _httpClient.SendAsync(new HttpRequestMessage(HttpMethod.Post, uri));
 			
 			return response.StatusCode;
 	    }
