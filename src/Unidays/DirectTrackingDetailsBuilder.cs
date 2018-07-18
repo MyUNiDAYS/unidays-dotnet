72dfc6e78cd6f5abc5e65637e09eeb545cfa93d6
jessica.white <jessica.white@myunidays.com>
2018-07-18 16:09:16 +0100

    Small refactor of builder

--- src/Unidays/DirectTrackingDetailsBuilder.cs
+++ src/Unidays/DirectTrackingDetailsBuilder.cs
@@ -86,24 +86,4 @@
 		    return this;
 	    }
 	}
-
-	public class DirectTrackingDetails
-	{
-		public string CustomerId { get; set; }
-		public string TransactionId { get; set; }
-		public string Currency { get; set; }
-
-		public string MemberId { get; set; }
-		public string Code { get; set; }
-		public decimal? OrderTotal { get; set; }
-		public decimal? ItemsUNiDAYSDiscount { get; set; }
-		public decimal? ItemsTax { get; set; }
-		public decimal? ShippingGross { get; set; }
-		public decimal? ShippingDiscount { get; set; }
-		public decimal? ItemsGross { get; set; }
-		public decimal? ItemsOtherDiscount { get; set; }
-		public decimal? UNiDAYSDiscountPercentage { get; set; }
-		public bool? NewCustomer { get; set; }
-		public string Signature { get; set; }
-	}
 }
